af80292e6fcbed72086fdf2c3b7665127d324522
Vilmor Somera <91754316+Vsomera@users.noreply.github.com>
2023-01-23 17:45:07 -0800

    Add files via upload

--- game.js
+++ game.js
@@ -1,54 +1,141 @@
-function animateButton(buttonColor) {  
-    // animates the selected button                     
-    $('#'+buttonColor).fadeOut(100).fadeIn(100);
+let level = 0
+
+let userClickedPattern = []
+var gamePattern = []
+const buttonColours = ["red", "yellow", "blue", "green"]
+
+function wrongAnswer() {
+    $("h1").text("wrong answer")
 }
 
+/********** Constantly checks if the user clicked pattern and the game pattern matches through out the game *************/
+function checkAnswer() {
+    if ((userClickedPattern == gamePattern)) {
+        level ++
+        userClickedPattern = []
+        setTimeout(function() {
+            nextSequence()
+            }, 1000)
+    }
+    else {
+        wrongAnswer()
+    }
+}
+
+/*********   Plays sound given the color of the button   *************/
 function playAudio(color) {                                 
-    // plays sound given the color of the button
-    switch(color) {
-        case "green":
-            var audio = new Audio("/sounds/green.mp3"); 
-            audio.play();
-            break;
-        case "blue":
-            var audio = new Audio("/sounds/blue.mp3"); 
-            audio.play();
-            break;
-        case "yellow":
-            var audio = new Audio("/sounds/yellow.mp3"); 
-            audio.play();
-            break;
-        case "red":
-            var audio = new Audio("/sounds/red.mp3");      
-            audio.play();
-            break;
-    };
-}
-
-function nextSequence() {
-    // generates a random number between 0 - 3
-    let randomNumber = Math.floor(Math.random() * 4) 
-    return randomNumber
+        var audio = new Audio("/sounds/"+ color +".mp3"); 
+        audio.play();
 };
 
-userClickedPattern = []
-gamePattern = []
-buttonColours = ["red", "blue", "green", "yellow"];
+/**********  Generates new sequence of colours when a user presses a button ****************/
+function nextSequence() {
+    $("h1").text("Level " + level)
+    var randomNumber = Math.floor(Math.random() * 4)
+    var randomColour = buttonColours[randomNumber]
+
+    gamePattern.push(randomColour)
+    playAudio(randomColour)
+    $("#"+randomColour).fadeOut(100).fadeIn(100);
+
+}
 
 
-var randomChosenColour = buttonColours[nextSequence()];
-gamePattern.push(randomChosenColour)
-
-
-
-
-for(i = 0; i < buttonColours.length; i++) {             // checks for which button is clicked and sends it to the user click pattern array
-    $("#"+buttonColours[i]).click(function() {
+/*********   Checks when a button is pressed (Collects user answer)  *************/
+for (let x in $(".btn")) {
+    $("#"+buttonColours[x]).click(function() {
         var userChosenColour = this.id
-        userClickedPattern.push(userChosenColour)
         playAudio(userChosenColour)
+        userClickedPattern.push(userChosenColour)
 
+        $("#"+userChosenColour).addClass("pressed")
+            setTimeout(function() {
+                $("#"+userChosenColour).removeClass("pressed")}, 100)
+    
+    
+
+    if (userClickedPattern.length == gamePattern.length) {
+        checkAnswer()}
     })
 }
+
+
+
+// for(i = 0; i < buttonColours.length; i++) {             // checks for which button is clicked and sends it to the user click pattern array
+//     $("#"+buttonColours[i]).click(function() {
+//         var userChosenColour = this.id
+//         userClickedPattern.push(userChosenColour)
+
+//         playAudio(userChosenColour)                     // plays audio and animates buttons when clicked
+//         $("#"+userChosenColour).addClass("pressed")
+//         setTimeout(function() {
+//             $("#"+userChosenColour).removeClass("pressed")
+//         }, 100)
+//     });
+// };
+
+
+
+
+// function animateButton(buttonColor) {  
+//     // animates the selected button                     
+//     $('#'+buttonColor).fadeOut(100).fadeIn(100);
+// }
+
+// function animatePattern(buttonColor) {
+//     // animates the game pattern
+//     $("#"+buttonColor).fadeOut(100).fadeIn(100);
+// }
+
+
+
+// function nextSequence() {
+//     // generates a random number between 0 - 3
+//     let randomNumber = Math.floor(Math.random() * 4) 
+//     return randomNumber
+// };
+
+// function checkAnswer(currentLevel) {
+//     // function will check if user input matches game pattern
+//     if (currentLevel[currentLevel.length - 1] == gamePattern[gamePattern.length - 1]) {
+//         console.log("success")
+//         setTimeout(function() {
+//             gamePattern.push(buttonColours[nextSequence()])
+//         })
+//     } 
+//     else {
+//         console.log("wrong")
+//     }
+// }
+
+// userClickedPattern = [];
+// gamePattern = [];
+// buttonColours = ["red", "blue", "green", "yellow"];
+
+// for(i = 0; i < buttonColours.length; i++) {             // checks for which button is clicked and sends it to the user click pattern array
+//     $("#"+buttonColours[i]).click(function() {
+//         var userChosenColour = this.id
+//         userClickedPattern.push(userChosenColour)
+
+//         checkAnswer(userClickedPattern)
+
+
+//         playAudio(userChosenColour)                     // plays audio and animates buttons when clicked
+//         $("#"+userChosenColour).addClass("pressed")
+//         setTimeout(function() {
+//             $("#"+userChosenColour).removeClass("pressed")
+//         }, 100)
+//     });
+// };
+
+
+// var randomChosenColour = buttonColours[nextSequence()];
+// gamePattern.push(randomChosenColour);
+// animatePattern(randomChosenColour)
+
+
+
+
+  
 
 
